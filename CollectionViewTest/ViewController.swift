52533912e6d9b5cd2eaf48ac0f0b53d220536540
Omer Hagage <ohagage@lightricks.com>
2022-04-28 11:16:33 +0300

    Initial Commit

---
+++ CollectionViewTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionViewTest
+//
+//  Created by Omer Hagage on 28/04/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
